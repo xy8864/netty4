aa616e3fdd2370d48ab0798c0beabafee96a6913
sustbeckham <yingchao.zyc@alibaba-inc.com>
2014-04-28 23:56:20 +0800

    a example about nio. thinking why server must handler client event.

--- example/src/main/java/io/netty/eatnetty/nio/NioServer.java
+++ example/src/main/java/io/netty/eatnetty/nio/NioServer.java
@@ -2,9 +2,11 @@ package io.netty.eatnetty.nio;
  
 import java.net.InetSocketAddress;
 import java.net.ServerSocket; 
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -41,15 +43,27 @@ public class NioServer {
 				SelectionKey key = iterKeys.next();
 				
 				if((key.readyOps() & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT){
-					System.out.println("accept.");
+					// 这里的调用accept也很重要。
+					// 表明serverChannel已经处理了该事件。select再次循环的时候就不会筛选到该事件了。否则会有无限的循环。
+					SocketChannel clientSocket = serverChannel.accept();
+					
+					clientSocket.configureBlocking(false);
+					clientSocket.register(select, SelectionKey.OP_READ);
+				} else if((key.readyOps() & SelectionKey.OP_READ) == SelectionKey.OP_READ){
+				    // System.out.println("f"); 
+					// 如果注释掉下边if代码块里边所有的内容，只留下上边这一句输出的话又会出现客户端打一个字，服务器不停
+					// 输出字符f的情况，这就是因为，如果服务器不处理过来的请求，selector就会一直可以筛选出这个感兴趣
+					// 的事件。然后做无限循环。
+					SocketChannel channel = (SocketChannel) key.channel();
+					
+					ByteBuffer buf = ByteBuffer.allocate(1024);
+					channel.read(buf);
+					
+					System.out.println(new String(buf.array()));
 				} else {
 					System.out.println("fuck, no accept.");
 				}
 				
-				// 删除当前正在遍历的这个对象是必须的。这里真是想吐槽JDK的这个设计。
-				// 如果不删除，再次循环的时候，select方法将不会阻塞(妈蛋，意思就是select()拿到的始终是最新的当前发送过来的事件，接收过的就不管了)。
-				// 但是select.selectedKeys拿的是所有的感兴趣的事件。包括以前已经处理过。
-				// 所以我们写的时候一般都手工remove掉对应的key。
 				// 不对，这里我理解的还是不对~~ TODO
 				iterKeys.remove();
 			}
